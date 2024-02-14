f414eaae329db212adf44f71339e292600595407
mnasani <mnasani79@gmail.com>
2024-02-14 17:23:38 +0300

    service point edit modal

--- resources/views/admin/expertservice/pointmodal.blade.php
+++ resources/views/admin/expertservice/pointmodal.blade.php
@@ -1,7 +1,7 @@
     <div class="modal-dialog modal-dialog-scrollable" role="document">
         <div class="modal-content modal-content-demo">
             <div class="modal-header">
-                <h6 class="modal-title">النقاط :(الخدمة:  {{ $expertservice->service->name }} -الخبير:{{ $expertservice->expert->first_name . ' ' . $expertservice->expert->last_name }})</h6> <button aria-label="Close" class="close"
+                <h6 class="modal-title">تعديل نقاط خدمة:  {{ $expertservice->service->name }}</h6> <button aria-label="Close" class="close"
                     data-dismiss="modal" type="button"><span aria-hidden="true">&times;</span></button>
             </div>
             <div class="modal-body">
@@ -10,10 +10,11 @@
                     <div class="col">
                         <div class="card  box-shadow-0">
                             <div class="card-body pt-4">
+                                <h6 class="modal-title">الخبير: {{ $expertservice->expert->first_name . ' ' . $expertservice->expert->last_name }}</h6>
                                 <form class="form-horizontal" name="point_form"
                                     action="{{ url('admin/service/point/save', $expertservice->id) }}" method="POST"
                                     id="point_form">
-                                    @csrf                        
+                                    @csrf
                                     <div class="form-group mb-3">
                                         <input type="text" class="form-control " value="{{ $expertservice->points }}" id="points" placeholder="{{ __('general.points') }}" name="points">
                                         <ul class="parsley-errors-list filled" >
@@ -28,8 +29,8 @@
                 <!-- row -->
             </div>
             <div class="modal-footer">
-                <button class="btn ripple btn-primary" type="submit" form="point_form" name="btn_save_point_form" id="btn_save_point_form"   
-                
+                <button class="btn ripple btn-primary" type="submit" form="point_form" name="btn_save_point_form" id="btn_save_point_form"
+
                    >حفظ</button>
                 <button class="btn ripple btn-secondary" data-dismiss="modal" name="btn_cancel_field"
                     id="btn_cancel_field" type="button"> إلغاء</button>
@@ -37,5 +38,5 @@
         </div>
     </div>
 <!--End Scroll with content modal -->
- 
+
 <script src="{{ URL::asset('assets/js/admin/pointmodal.js') }}"></script>
