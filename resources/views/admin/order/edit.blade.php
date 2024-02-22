6602ada6f099a488d12e36517fd6a4528488ddc9
mnasani <mnasani79@gmail.com>
2024-02-22 16:06:26 +0300

    jquery image gallery

--- resources/views/admin/order/edit.blade.php
+++ resources/views/admin/order/edit.blade.php
@@ -10,6 +10,7 @@
     <link href="{{ URL::asset('assets/plugins/spectrum-colorpicker/spectrum.css') }}" rel="stylesheet">
 
     <link href="{{ URL::asset('assets/css/admin/content.css') }}" rel="stylesheet">
+    <link href="{{ URL::asset('assets/css/simple-lightbox.css') }}" rel="stylesheet">
 @endsection
 @section('page-header')
     <!-- breadcrumb -->
@@ -48,14 +49,20 @@
                                     src="{{ $valueService->svg_path }}">
                                 {{ ' ' . $valueService->tooltipe }}</span>{{ ' ' . $valueService->value_conv }}</p>
                     @endforeach
-
-                    @foreach ($selectedservice->valueServices->where('type', 'image') as $valueService)
-                        <div class="pd-20 clearfix">
+                    <div class="gallery">
+                        @foreach ($selectedservice->valueServices->where('type', 'image') as $valueService)
+                        <a href="{{ $valueService->full_path_conv }}" class="big" rel="rel1">
+                            <img src="{{ $valueService->full_path_conv }}" alt="" title="">
+                        </a>
+                        @endforeach
+                    </div>
+                        <div class="bd pd-20 clearfix">
+                            @foreach ($selectedservice->valueServices->where('type', 'image') as $valueService)
                             <img alt="" id="imgshow"
-                                class="rounded img-thumbnail wd-100p wd-sm-200 float-sm-right  mg-t-10 mg-sm-t-0"
+                                class="rounded float-sm-right wd-100p wd-sm-200 ml-3"
                                 src="{{ $valueService->full_path_conv }}">
+                            @endforeach
                         </div>
-                    @endforeach
                     @foreach ($selectedservice->valueServices->where('type', 'record') as $valueService)
                         <label class="col-sm-12 ">تسجيل صوتي</label>
                         <audio controls class="col-sm-12 ">
@@ -147,9 +154,136 @@
     <script src="{{ URL::asset('assets/js/admin/validate.js') }}"></script>
     <script src="{{ URL::asset('assets/js/admin/order.js') }}"></script>
     <script src="{{ URL::asset('assets/js/form-elements.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/simple-lightbox.js') }}"></script>
+    <!-- For legacy browsers -->
+    <script src="{{ URL::asset('assets/js/simple-lightbox.legacy.min.js') }}"></script>
+    <!-- As A jQuery Plugin -->
+    <script src="{{ URL::asset('assets/js/jquery-3.7.1.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/simple-lightbox.jquery.min.js') }}"></script>
     <script>
         var emptyimg = "{{ URL::asset('assets/img/photos/1.jpg') }}";
 
         $('#expertdate').datepicker("option", "altFormat", "yy-mm-dd");
+
+        var lightbox = $('.gallery a').simpleLightbox({
+            // default source attribute
+            sourceAttr: 'href',
+
+            // shows fullscreen overlay
+            overlay: true,
+
+            // shows loading spinner
+            spinner: true,
+
+            // shows navigation arrows
+            nav: true,
+
+            // text for navigation arrows
+            navText: ['&larr;','&rarr;'],
+
+            // shows image captions
+            captions: true,
+            captionDelay: 0,
+            captionSelector: 'img',
+            captionType: 'attr',
+            captionPosition: 'bottom',
+            captionClass: '',
+            captionHTML: false,
+
+            // captions attribute (title or data-title)
+            captionsData: 'title',
+
+            // shows close button
+            close: true,
+
+            // text for close button
+            closeText: 'X',
+
+            // swipe up or down to close gallery
+            swipeClose: true,
+
+            // show counter
+            showCounter: true,
+
+            // file extensions
+            fileExt: 'png|jpg|jpeg|gif',
+
+            // weather to slide in new photos or not, disable to fade
+            animationSlide: true,
+
+            // animation speed in ms
+            animationSpeed: 250,
+
+            // image preloading
+            preloading: true,
+
+            // keyboard navigation
+            enableKeyboard: true,
+
+            // endless looping
+            loop:  true,
+
+            // group images by rel attribute of link with same selector
+            rel: false,
+
+            // closes the lightbox when clicking outside
+            docClose:  true,
+
+            // how much pixel you have to swipe
+            swipeTolerance: 50,
+
+            // lightbox wrapper Class
+            className: 'simple-lightbox',
+
+            // width / height ratios
+            widthRatio: 0.8,
+            heightRatio: 0.9,
+
+            // scales the image up to the defined ratio size
+            scaleImageToRatio: false,
+
+            // disable right click
+            disableRightClick: false,
+
+            // disable page scroll
+            disable<a href="https://www.jqueryscript.net/tags.php?/Scroll/">Scroll</a>: true,
+
+            // show an alert if image was not found
+            alertError:  true,
+
+            // alert message
+            alertErrorMessage: 'Image not found, next image will be loaded',
+
+            // additional HTML showing inside every image
+            additionalHtml: false,
+
+            // enable history back closes lightbox instead of reloading the page
+            history: true,
+
+            // time to wait between slides
+            throttleInterval: 0,
+
+            // Pinch to <a href="https://www.jqueryscript.net/zoom/">Zoom</a> feature for touch devices
+            doubleTapZoom: 2,
+            maxZoom: 10,
+
+            // adds class to html element if lightbox is open
+            htmlClass: 'has-lightbox',
+
+            // RTL mode
+            rtl: false,
+
+            // elements with this class are fixed and get the right padding when lightbox opens
+            fixedClass: 'sl-fixed',
+
+            // fade speed in ms
+            fadeSpeed: 300,
+
+            // whether to uniqualize images
+            uniqueImages: true,
+
+            // focus the lightbox on open to enable tab control
+            focus: true,
+        });
     </script>
 @endsection
