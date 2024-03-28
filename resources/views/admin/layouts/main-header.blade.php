a5d12c52b99a35a2a5f6891cc1a1f2ebfa4c5b5e
najims2016 <>
2024-03-29 01:45:44 +0300

    Update main-header.blade.php

--- resources/views/admin/layouts/main-header.blade.php
+++ resources/views/admin/layouts/main-header.blade.php
@@ -3,10 +3,10 @@
 				<div class="container-fluid">
 					<div class="main-header-left ">
 						<div class="responsive-logo">
-							<a href="{{ url('/' . $page='index') }}"><img src="{{URL::asset('assets/img/brand/logo-title.svg')}}" class="logo-1" alt="logo"></a>
-							<a href="{{ url('/' . $page='index') }}"><img src="{{URL::asset('assets/img/brand/logo-title.svg')}}" class="dark-logo-1" alt="logo"></a>
-							<a href="{{ url('/' . $page='index') }}"><img src="{{URL::asset('assets/img/brand/logo-title.svg')}}" class="logo-2" alt="logo"></a>
-							<a href="{{ url('/' . $page='index') }}"><img src="{{URL::asset('assets/img/brand/logo-title.svg')}}" class="dark-logo-2" alt="logo"></a>
+							<a href="{{route('admin')}}"><img src="{{URL::asset('assets/img/brand/logo-title.svg')}}" class="logo-1" alt="logo"></a>
+							<a href="{{route('admin')}}"><img src="{{URL::asset('assets/img/brand/logo-title.svg')}}" class="dark-logo-1" alt="logo"></a>
+							<a href="{{route('admin')}}"><img src="{{URL::asset('assets/img/brand/logo-title.svg')}}" class="logo-2" alt="logo"></a>
+							<a href="{{route('admin')}}"><img src="{{URL::asset('assets/img/brand/logo-title.svg')}}" class="dark-logo-2" alt="logo"></a>
 						</div>
 						<div class="app-sidebar__toggle" data-toggle="sidebar">
 							<a class="open-toggle" href="#"><i class="header-icon fe fe-align-left" ></i></a>
